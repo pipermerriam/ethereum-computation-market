617ce599e202638f33f9483f4551d1e805a88be3
Piper Merriam <pipermerriam@gmail.com>
2016-02-01 13:11:46 -0700

    reset project

--- contracts/Marketplace.sol
+++
@@ -1,9 +0,0 @@
-import "libraries/ComputationLib.sol";
-
-
-contract Marketplace {
-        function register_computation_contract(address verifier_address) returns (address) {
-                Computation computation = new Computation(verifier_address);
-                return address(computation);
-        }
-}
